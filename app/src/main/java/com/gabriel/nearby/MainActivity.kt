73797ccd6742e93c7c69e42e08e4f1c310431682
Gabriel Augusto <Gabriel.afg182@gmail.com>
2024-12-12 15:22:31 -0300

    Aula 2 Finalizada

--- app/src/main/java/com/gabriel/nearby/MainActivity.kt
+++ app/src/main/java/com/gabriel/nearby/MainActivity.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.gabriel.nearby.data.model.Market
+import com.gabriel.nearby.ui.screen.HomeScreen
+import com.gabriel.nearby.ui.screen.MarketDetailsScreen
+import com.gabriel.nearby.ui.screen.SplashScreen
+import com.gabriel.nearby.ui.screen.WelcomeScreen
+import com.gabriel.nearby.ui.screen.route.Home
+import com.gabriel.nearby.ui.screen.route.Splash
+import com.gabriel.nearby.ui.screen.route.Welcome
 import com.gabriel.nearby.ui.theme.NearbyTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +24,43 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NearbyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Splash
                 ) {
-                    Greeting("Android")
+                    composable<Splash> {
+                        SplashScreen(
+                            modifier = Modifier.fillMaxSize(),
+                            onNavigateToWelcome = {
+                                navController.navigate(Welcome)
+                            }
+                        )
+                    }
+                    composable<Welcome> {
+                        WelcomeScreen(onNavigateToHome = {
+                            navController.navigate(Home)
+                        })
+                    }
+                    composable<Home> {
+                        HomeScreen(onNavigateToMarketDetails = {selectedMarket ->
+                            navController.navigate(selectedMarket)
+                        })
+                    }
+                    composable<Market>{
+                        val selectedMarket = it.toRoute<Market>()
+
+                        MarketDetailsScreen(
+                            market = selectedMarket,
+                            modifier = Modifier,
+                            onNavigateBack = {
+                                navController.popBackStack()
+                            }
+                        )
+                    }
                 }
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NearbyTheme {
-        Greeting("Android")
     }
 }
